864ff29a100f7f045dc148c9a9697bfeafc0c5d5
AzulCura <azulcura1@gmail.com>
2024-06-01 14:30:08 -0300

    Dump

--- database/Dump0106.sql
+++ database/Dump0106.sql
@@ -41,6 +41,32 @@ INSERT INTO `administrators` VALUES (120120,'administrador@utn.com.ar','Admin123
 UNLOCK TABLES;
 
 --
+-- Table structure for table `careers`
+--
+
+DROP TABLE IF EXISTS `careers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `careers` (
+  `idCareers` int NOT NULL AUTO_INCREMENT,
+  `nameCareers` varchar(70) NOT NULL,
+  `InstitutionCareers` varchar(45) NOT NULL,
+  `Type` enum('Grado','Tecnicatura') NOT NULL,
+  PRIMARY KEY (`idCareers`),
+  UNIQUE KEY `UNIQUE` (`nameCareers`,`InstitutionCareers`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `careers`
+--
+
+LOCK TABLES `careers` WRITE;
+/*!40000 ALTER TABLE `careers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `careers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `company`
 --
 
@@ -71,6 +97,30 @@ LOCK TABLES `company` WRITE;
 /*!40000 ALTER TABLE `company` DISABLE KEYS */;
 INSERT INTO `company` VALUES ('12346789','Prueba','Prueba','Prueba123','prueba@gmail.com',NULL,NULL,NULL,NULL,'iqARBnZ2PkSMCrkiB02tXg==;guUawXI4jsdSTNTD0o3OlBZjnZ1jv3smRLvj5XHyDVo=','Pending');
 /*!40000 ALTER TABLE `company` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `skills`
+--
+
+DROP TABLE IF EXISTS `skills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `skills` (
+  `idSkills` int NOT NULL AUTO_INCREMENT,
+  `descriptionSkills` varchar(45) NOT NULL,
+  PRIMARY KEY (`idSkills`),
+  UNIQUE KEY `descriptionSkills_UNIQUE` (`descriptionSkills`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `skills`
+--
+
+LOCK TABLES `skills` WRITE;
+/*!40000 ALTER TABLE `skills` DISABLE KEYS */;
+/*!40000 ALTER TABLE `skills` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -126,6 +176,93 @@ LOCK TABLES `student` WRITE;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `studentscareers`
+--
+
+DROP TABLE IF EXISTS `studentscareers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `studentscareers` (
+  `idStudents` int NOT NULL,
+  `idCareers` int NOT NULL,
+  `enrollmentDate` date NOT NULL,
+  `graduationDate` date DEFAULT NULL,
+  `isComplete` enum('SI','NO') NOT NULL,
+  PRIMARY KEY (`idStudents`,`idCareers`),
+  KEY `StudentsCareers_idCareers_idx` (`idCareers`),
+  CONSTRAINT `StudentsCareers_idCareers` FOREIGN KEY (`idCareers`) REFERENCES `careers` (`idCareers`),
+  CONSTRAINT `StudentsCareers_idStudents` FOREIGN KEY (`idStudents`) REFERENCES `student` (`Legajo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `studentscareers`
+--
+
+LOCK TABLES `studentscareers` WRITE;
+/*!40000 ALTER TABLE `studentscareers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `studentscareers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `studentsexperience`
+--
+
+DROP TABLE IF EXISTS `studentsexperience`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `studentsexperience` (
+  `idStudentsExperience` int NOT NULL AUTO_INCREMENT,
+  `idStudents` int NOT NULL,
+  `description` varchar(150) NOT NULL,
+  `company` varchar(45) NOT NULL,
+  `startDate` date NOT NULL,
+  `endDate` date DEFAULT NULL,
+  `IdSkills` int DEFAULT NULL,
+  PRIMARY KEY (`idStudentsExperience`),
+  KEY `StudentsExperiencie_IdStudents_idx` (`idStudents`),
+  KEY `StudentsExperiencie_IdSkills_idx` (`IdSkills`),
+  CONSTRAINT `StudentsExperiencie_IdSkills` FOREIGN KEY (`IdSkills`) REFERENCES `skills` (`idSkills`),
+  CONSTRAINT `StudentsExperiencie_IdStudents` FOREIGN KEY (`idStudents`) REFERENCES `student` (`Legajo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `studentsexperience`
+--
+
+LOCK TABLES `studentsexperience` WRITE;
+/*!40000 ALTER TABLE `studentsexperience` DISABLE KEYS */;
+/*!40000 ALTER TABLE `studentsexperience` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `studentsskills`
+--
+
+DROP TABLE IF EXISTS `studentsskills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `studentsskills` (
+  `idStudents` int NOT NULL,
+  `idSkills` int NOT NULL,
+  PRIMARY KEY (`idStudents`,`idSkills`),
+  KEY `StudentsSkills_idSkills_idx` (`idSkills`),
+  CONSTRAINT `StudentsSkills_idSkills` FOREIGN KEY (`idSkills`) REFERENCES `skills` (`idSkills`),
+  CONSTRAINT `StudentsSkills_idStudents` FOREIGN KEY (`idStudents`) REFERENCES `student` (`Legajo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `studentsskills`
+--
+
+LOCK TABLES `studentsskills` WRITE;
+/*!40000 ALTER TABLE `studentsskills` DISABLE KEYS */;
+/*!40000 ALTER TABLE `studentsskills` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -136,4 +273,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-27 22:50:15
+-- Dump completed on 2024-06-01 14:12:50
