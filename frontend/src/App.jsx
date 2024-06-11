3c5e7ec7b4898a4e6a07dc0fe87981e5e3fb5b93
Marcelo Fracassi <marcelo.fracassi@ecloudsolutions.com>
2024-06-11 06:15:30 -0300

    App fixed conflics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,6 @@ import { ThemeProvider } from "./components/context/theme-context/theme-context"
 import { ChakraProvider } from "@chakra-ui/react";
 import AdminStudent from "./components/admin/admin-student";
 import AdminCompany from "./components/admin/admin-company";
-<<<<<<< HEAD
 import CompanyPage from "./pages/company-page/company-page";
 import { CompanyProfile } from "./pages/company-page/company-profile";
 import JobPostulationsCompany from "./pages/company-page/job-opportunities";
@@ -23,45 +22,12 @@ import { StudentPage } from "./pages/student-page/student-page";
 import { JobOpportunities } from "./pages/student-page/job-opportunities";
 import { JobPostulations } from "./pages/student-page/job-postulations";
 import { StudentProfile } from "./pages/student-page/student-profile";;
-=======
 import { AuthProvider } from "./components/context/AuthProvider";
->>>>>>> 16f3930892f66ee4c94e33db7ddcc5ccfb99a832
 
 function App() {
   const [previousPath, setPreviousPath] = useState("/");
 
   return (
-<<<<<<< HEAD
-    <ThemeProvider>
-      <ChakraProvider>
-        <Router>
-          <Routes>
-            <Route
-              path="/"
-              element={<HomePage />}
-              onChange={(params) => setPreviousPath(params.location.pathname)}
-            />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/admin-page" element={<AdminPage />} />
-            <Route path="/admin/student" element={<AdminStudent />} />
-            <Route path="/admin/company" element={<AdminCompany />} />
-            <Route path="/student" element={<StudentPage />} />
-            <Route path="/student/oportunities" element={<JobOpportunities />} />
-            <Route path="/student/postulations" element={<JobPostulations />} />
-            <Route path="/student/my-profile" element={<StudentProfile />} />
-            <Route path="/company" element={<CompanyPage />} />
-            {/* <Route path="/company/oportunities" element={<JobOpportunities />} /> */}
-            <Route path="/company/postulations" element={<JobPostulationsCompany />} />
-            <Route path="/company/my-profile" element={<CompanyProfile />} />
-            <Route path="/AccountAuth" element={<AccountAuth />} />
-            <Route path="/login" element={<Navigate to={previousPath} />} />
-          </Routes>
-          <FooterPage />
-        </Router>
-      </ChakraProvider>
-    </ThemeProvider>
-=======
     <AuthProvider>
       <ThemeProvider>
         <ChakraProvider>
@@ -74,11 +40,17 @@ function App() {
               />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
-              <Route path="/student" element={<StudentPage />} />
-              <Route path="/company" element={<CompanyPage />} />
               <Route path="/admin-page" element={<AdminPage />} />
               <Route path="/admin/student" element={<AdminStudent />} />
               <Route path="/admin/company" element={<AdminCompany />} />
+              <Route path="/student" element={<StudentPage />} />
+              <Route path="/student/oportunities" element={<JobOpportunities />} />
+              <Route path="/student/postulations" element={<JobPostulations />} />
+              <Route path="/student/my-profile" element={<StudentProfile />} />
+              <Route path="/company" element={<CompanyPage />} />
+              {/* <Route path="/company/oportunities" element={<JobOpportunities />} /> */}
+              <Route path="/company/postulations" element={<JobPostulationsCompany />} />
+              <Route path="/company/my-profile" element={<CompanyProfile />} />
               <Route path="/AccountAuth" element={<AccountAuth />} />
               <Route path="/login" element={<Navigate to={previousPath} />} />
             </Routes>
@@ -87,7 +59,6 @@ function App() {
         </ChakraProvider>
       </ThemeProvider>
     </AuthProvider>
->>>>>>> 16f3930892f66ee4c94e33db7ddcc5ccfb99a832
   );
 }
 
